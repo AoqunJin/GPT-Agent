4c42c5d2b945dafd9cba931085b74660745e30c0
Aoqun Jin <aojiaojiao@foxmail.com>
2024-01-24 16:37:18 +0800

    Add files via upload

--- data_collection/metaworld_data.py
+++ data_collection/metaworld_data.py
@@ -10,11 +10,6 @@ from metaworld.policies import *
 import metaworld.envs.mujoco.env_dict as _env_dict
 
 
-# TODO arg dir
-DATA_DIR = "./demos"
-# TODO parallel
-
-
 def get_policy_names(env_names: List[str]) -> List[str]:
     """Generate policy names based on environment names.
 
@@ -38,7 +33,7 @@ def get_policy_names(env_names: List[str]) -> List[str]:
     return policy_names
 
 
-def get_all_envs() -> Tuple[List[str], List[Type]]:
+def get_all_envs(n_envs: int = 50) -> Tuple[List[str], List[Type]]:
     """Get all Metaworld 50 environments.
 
     Returns:
@@ -49,7 +44,7 @@ def get_all_envs() -> Tuple[List[str], List[Type]]:
     for env_name in _env_dict.MT50_V2:
         env_names.append(env_name)
         envs.append(_env_dict.MT50_V2[env_name])
-    return env_names, envs
+    return env_names[:n_envs], envs[:n_envs]
 
 
 def file_maker(folder: Optional[str] = None):
@@ -84,17 +79,18 @@ def data_saver(file: str, data: Any):
 def collect_trail(env, policy, env_name: str, trail: int, 
                   use_rgb: bool = True, res: tuple = (640, 480), 
                   camera_name: str = "corner", num_step: int = 501):
-    """_summary_
+    """Collect data for a single trial in the specified environment using the given policy.
 
     Args:
-        env (_type_): _description_
-        policy (_type_): _description_
-        env_name (str): _description_
-        trail (int): _description_
-        use_rgb (bool, optional): _description_. Defaults to True.
-        res (tuple, optional): _description_. Defaults to (640, 480).
-        camera_name (str, optional): _description_. Defaults to "corner".
-        num_step (int, optional): _description_. Defaults to 501.
+        env (Type): The environment instance to collect data from.
+        policy (Type): The policy used for data collection.
+        env_name (str): The name of the environment.
+        trail (int): The trial number.
+        use_rgb (bool, optional): Flag indicating whether to use RGB observations. Defaults to True.
+        res (tuple, optional): Resolution of RGB observations. Defaults to (640, 480).
+        camera_name (str, optional): Name of the camera view. One of ["corner", "topview", "behindGripper", "gripperPOV"].
+                                     Defaults to "corner".
+        num_step (int, optional): Number of steps in the trial. Defaults to 501.
     """
     obss, acts, rews, dones = [], [], [], []
     obs = env.reset()
@@ -131,6 +127,7 @@ def collect_trail(env, policy, env_name: str, trail: int,
     }
 
     data_saver(str(env_name) + "/collect_" + str(trail), data)
+    del data
 
 
 def collect_demos(env_names: List[str], envs: List[Type], policy_names: List[str],
@@ -148,7 +145,7 @@ def collect_demos(env_names: List[str], envs: List[Type], policy_names: List[str
                           Defaults to "corner".
         num_trail (int): Number of trials for data collection. Defaults to 2000.
         num_step (int): Number of steps per trial. Defaults to 501.
-        num_workers
+        num_workers (int): Number of worker processes for parallel data collection. Defaults to 5.
     """
     with multiprocessing.Pool(processes=num_workers) as pool:
         for i in range(len(env_names)):
@@ -164,6 +161,8 @@ def collect_demos(env_names: List[str], envs: List[Type], policy_names: List[str
             file_maker(str(env_name))
 
             for trail in range(num_trail):
+
+                # TODO optimize
                 pool.apply_async(
                     func=collect_trail,
                     args=(env, policy, env_name, trail, use_rgb, 
@@ -176,10 +175,38 @@ def collect_demos(env_names: List[str], envs: List[Type], policy_names: List[str
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Collect meta-world demonstration.")
 
-    parser.add_argument("--num_trail", type=int, default=20, help="Number of trials for data collection.")
-    parser.add_argument("--num_workers", type=int, default=3, help="Number of worker processes.")
+    # parser.add_argument("--env_names", nargs="+", help="List of environment names.")
+    # parser.add_argument("--envs", nargs="+", help="List of environment classes.")
+    # parser.add_argument("--policy_names", nargs="+", help="List of policy names.")
+    parser.add_argument("--n_envs", type=int, default=50, help="Number of collect environment.")
+    parser.add_argument("--use_rgb", action="store_true", help="Flag indicating whether to use RGB observations.")
+    parser.add_argument("--res", nargs=2, type=int, default=[224, 224], help="Resolution of RGB observations.")
+    parser.add_argument("--camera_name", choices=["corner", "topview", "behindGripper", "gripperPOV"], default="corner", help="Name of the camera view.")
+    parser.add_argument("--num_trail", type=int, default=2000, help="Number of trials for data collection.")
+    parser.add_argument("--num_step", type=int, default=501, help="Number of steps per trial.")
+    parser.add_argument("--num_workers", type=int, default=1, help="Number of worker processes.")
+    parser.add_argument("--data_dir", type=str, default="data", help="Directory to store collected data.")
     args = parser.parse_args()
 
-    env_names, envs = get_all_envs()
+    DATA_DIR = args.data_dir
+    env_names, envs = get_all_envs(args.n_envs)
     policy_names = get_policy_names(env_names)
-    collect_demos(env_names, envs, policy_names, num_trail=args.num_trail, num_workers=args.num_workers)
+
+    print("+" * 30, "parser", "+" * 30)
+    print("n_envs", args.n_envs)
+    print("env_names", env_names)
+    print("policy_names", policy_names)    
+    print("use_rgb", args.use_rgb)
+    print("res", tuple(args.res))
+    print("camera_name", args.camera_name)
+    print("num_trail", args.num_trail)
+    print("num_step", args.num_step)
+    print("num_trail", args.num_trail)
+    print("num_workers", args.num_workers)
+    print("data_dir", args.data_dir)
+    print("+" * 30, "parser", "+" * 30)
+
+    collect_demos(env_names, envs, policy_names, use_rgb=args.use_rgb, res=tuple(args.res),
+                  camera_name=args.camera_name, num_trail=args.num_trail, num_step=args.num_step,
+                  num_workers=args.num_workers)
+    print("All finished!")
